03eb28196082482c0b435cf57180b951d4df31dd
ianfuentes412 <ianfuentes412@users.noreply.github.com>
2020-07-16 11:47:29 -0700

    Added Test for getPosition()

--- driver.java
+++ driver.java
@@ -1,7 +1,17 @@
-public class Driver {
-      public static void main (String [] args)
-        {
-        
-        }
-        
-       }
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Work PC
+ */
+public class driver {
+    public static void main (String[] args)
+    {
+        HappyPig pig = new HappyPig("Bell","Red");
+        pig.getPosition();
+    }
+}
