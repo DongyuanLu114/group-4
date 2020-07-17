73a9e5dbae4036ce41813f97e2c768571801c9e2
Young-lim <67382580+Young-lim@users.noreply.github.com>
2020-07-16 22:03:03 -0700

    Update LoudGoose.java
    synchronized constructor

--- LoudGoose.java
+++ LoudGoose.java
@@ -1,16 +1,15 @@
 /*java.LoudGoose
- * Author: Young Lim Ko Park (T00051136)
+ Group 4
  */
 
 public class LoudGoose extends WaterFowl
 {
-     public LoudGoose(String name, String color, boolean flying)
+     public LoudGoose(String name, String color)
      {
           super(name, color);
-          this.flying = flying;
      }
      
-    public void makeSound()
+     public void makeSound()
      {
           System.out.println(name + " says I'm loud");
      }
