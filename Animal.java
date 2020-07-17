847b2bcde8f313c5a95427997715b4679afae447
Young-lim <67382580+Young-lim@users.noreply.github.com>
2020-07-16 21:46:58 -0700

    Update Animal.java
    fixed hit()

--- Animal.java
+++ Animal.java
@@ -1,7 +1,7 @@
 /*java.Animal
  *Group 4
  */
-  
+
 import java.util.Random;
 
 interface teleport
@@ -60,14 +60,20 @@ public abstract class Animal implements teleport
      
      public  void hit()
      {          
-          health -= 1;
           if (health == 0)
-          {
-               count --;
-               System.out.println(name + " got hit and is gone");
+          {               
+               System.out.println(name + " is already dead");
+               //break;
           }
           else
+          {
+               health -= 1;
                System.out.println(name + " got hit and now has " + health + " health left");
+               if (health == 0)
+               {
+                    count--;
+               }
+          }
      }  
      
      @Override
@@ -95,37 +101,39 @@ public abstract class Animal implements teleport
      }
      
      Random rand = new Random();
-
-          int x1 = 0;
-          int x2 = 0;
-          int y1 = 0;
-          int y2 = 0;
+     
+     int x1 = 0;
+     int x2 = 0;
+     int y1 = 0;
+     int y2 = 0;
+     
+     public void teleBunny()
+     {
+          x1 = rand.nextInt(500);
+          y1 = rand.nextInt(1000);
           
-          public void teleBunny()
+          if (health > 1)
           {
-               x1 = rand.nextInt(500);
-               y1 = rand.nextInt(1000);
-
+               System.out.println("The Bunny move to " + x1 + " , " + y1);
+          }
+          else
+          {
+               System.out.println("Sorry, the bunny is dead");
+          }
+     }
+          
+          public void teleDuck()
+          {
+               x2 = rand.nextInt((500)+500);
+               y2 = rand.nextInt(1000);
+               
                if (health > 1)
                {
-                    System.out.println("The Bunny move to " + x1 + " , " + y1);
+                    System.out.println("The Duck moves to " + x2 + " , " + y2);
                }
                else
                {
-                    System.out.println("Sorry, the bunny is dead");
+                    System.out.println("Sorry the duck is dead");
                }
-               
-               public void teleDuck
-               {
-                    x2 = rand.nextInt((500)+500);
-                    y2 = rand.nextInt(1000);
-
-                    if (health > 1)
-                    {
-                         System.out.println("The Duck moves to " + x2 + " , " + y2);
-                    }
-                    else
-                    {
-                         System.out.println("Sorry the duck is dead");
-                    }
+          }
 }
